12e6a64a77bff65c534fb83251c02446c040d782
Jithin Joshi <jithinjoshi2000@gmail.com>
2023-06-14 23:04:27 +0530

    message controller error solved

--- Server/controller/messageController.js
+++ Server/controller/messageController.js
@@ -6,32 +6,9 @@ export const newMessage = async (req, res) => {
     const { conversationId, sender, text } = req.body;
     const newMsg = new Message({ conversationId, sender, text });
     const messages = await newMsg.save();
-    if (sender === 'user') {
-      // Find the associated appointment
-      const appointment = await Appointment.findOne({ _id: conversationId });
-
-      // Check if the appointment exists
-      if (appointment) {
-        // Increment the chatLimit
-        appointment.chatLimit += 1;
-
-        // Check if the chatLimit has reached the maximum allowed messages
-        if (appointment.chatLimit >= 20) {
-          // Reset the chatLimit and update the appointment
-          appointment.chatLimit = 0;
-          await appointment.save();
-
-          // Return the response indicating the limit has been reached
-          return res.status(200).send({ success: true, message: 'Chat limit reached', messages });
-        }
-
-        // Update the appointment with the incremented chatLimit
-        await appointment.save();
-      }
-    }
     return res.status(200).send({ success: true, message: 'New Message Successful', messages });
   } catch (error) {
-
+    
     return res.status(500).send({ success: false, message: 'Internal Server Error' });
   }
 };
