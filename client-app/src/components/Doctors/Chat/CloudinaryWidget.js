ebf538982a6e45d91fcbc87ca19de6d39c0a73ce
Jithin Joshi <jithinjoshi2000@gmail.com>
2023-06-20 09:47:41 +0530

    chat issue resolved

--- client-app/src/components/Doctors/Chat/CloudinaryWidget.js
+++ client-app/src/components/Doctors/Chat/CloudinaryWidget.js
@@ -1,33 +1,42 @@
 import React, { useEffect } from "react";
 
-const CloudinaryWidget = ({currentUserId,chat, newMessages, setSendMessage, setMessages}) => {
+const CloudinaryWidget = ({
+  currentUserId,
+  chat,
+  newMessages,
+  setSendMessage,
+  setMessages
+}) => {
   useEffect(() => {
     const cloudName = "dpswips7e"; // replace with your own cloud name
     const uploadPreset = "webDoc"; // replace with your own upload preset
 
-
     var myWidget = window.cloudinary.createUploadWidget(
       {
         cloudName: cloudName,
-        uploadPreset: uploadPreset
+        uploadPreset: uploadPreset,
+        multiple: false, // Allow only a single file to be uploaded
+        resourceType: "image" // Restrict to images only
       },
       async (error, result) => {
         if (!error && result && result.event === "success") {
-          const message = {
-            sender: currentUserId,
-            text: {type:"image", data: result.info.secure_url},
-            conversationId: chat?._id,
-          };
-        
-          try {
-            const { data } = await newMessages(message);
-            setMessages((prevMessages) => [...prevMessages, data?.messages]);
-          } catch (error) {
-            return error;
+          if (result.info.resource_type === "image") { // Validate if the uploaded file is an image
+            const message = {
+              sender: currentUserId,
+              text: { type: "image", data: result.info.secure_url },
+              conversationId: chat?._id
+            };
+
+            try {
+              const { data } = await newMessages(message);
+              setMessages((prevMessages) => [...prevMessages, data?.messages]);
+            } catch (error) {
+              return error;
+            }
+
+            const receiverId = chat.members.find((id) => id !== currentUserId);
+            setSendMessage({ ...message, receiverId });
           }
-        
-          const recieverId = chat.members.find((id) => id !== currentUserId);
-          setSendMessage({ ...message, recieverId });
         }
       }
     );
@@ -36,19 +45,15 @@ const CloudinaryWidget = ({currentUserId,chat, newMessages, setSendMessage, setM
       myWidget.open();
     };
 
-    document.getElementById("upload_widget").addEventListener(
-      "click",
-      handleClick,
-      false
-    );
+    document
+      .getElementById("upload_widget")
+      .addEventListener("click", handleClick, false);
 
     // Cleanup function
     return () => {
-      document.getElementById("upload_widget").removeEventListener(
-        "click",
-        handleClick,
-        false
-      );
+      document
+        .getElementById("upload_widget")
+        .removeEventListener("click", handleClick, false);
     };
   }, []);
 
